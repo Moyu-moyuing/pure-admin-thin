2d77b60a858ea1c86492327b4e0687583ac5a0d8
Moyu-moyuing <1531872149@qq.com>
2023-07-23 06:13:12 +0800

    feat: 增加知识图谱节点信息面板功能

---
+++ src/views/knowledgeGraph/components/nodePanel.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+defineOptions({
+  name: "NodePanel"
+});
+</script>
+
+<template>
+  <el-card>
+    <h2>节点信息面板</h2>
+    <div>这是模拟数据</div>
+    <div>这是模拟数据</div>
+    <div>这是模拟数据</div>
+    <div>这是模拟数据</div>
+    <div>这是模拟数据</div>
+    <div>这是模拟数据</div>
+    <div>这是模拟数据</div>
+    <div>这是模拟数据</div>
+    <div>这是模拟数据</div>
+    <div>这是模拟数据</div>
+    <div>这是模拟数据</div>
+    <div>这是模拟数据</div>
+    <div>这是模拟数据</div>
+    <div>这是模拟数据</div>
+    <div>这是模拟数据</div>
+    <div>这是模拟数据</div>
+  </el-card>
+</template>
