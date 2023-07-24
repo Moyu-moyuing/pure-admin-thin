4bbecd4dde9a517bde390da5d25e057655a46e15
Moyu-moyuing <1531872149@qq.com>
2023-07-24 20:35:30 +0800

    perf: 简单页面修改

--- src/views/knowledgeGraph/components/nodePanel.vue
+++ src/views/knowledgeGraph/components/nodePanel.vue
@@ -1,3 +1,9 @@
+<!-- 
+ * @description: //TODO
+ * @fileName: nodePanel.vue 
+ * @author: Moyu-moyuing
+ * @date: 2023-07-24 13:24:42
+ -->
 <script setup lang="ts">
 defineOptions({
   name: "NodePanel"
@@ -6,22 +12,24 @@ defineOptions({
 
 <template>
   <el-card>
-    <h2>节点信息面板</h2>
-    <div>这是模拟数据</div>
-    <div>这是模拟数据</div>
-    <div>这是模拟数据</div>
-    <div>这是模拟数据</div>
-    <div>这是模拟数据</div>
-    <div>这是模拟数据</div>
-    <div>这是模拟数据</div>
-    <div>这是模拟数据</div>
-    <div>这是模拟数据</div>
-    <div>这是模拟数据</div>
-    <div>这是模拟数据</div>
-    <div>这是模拟数据</div>
-    <div>这是模拟数据</div>
-    <div>这是模拟数据</div>
-    <div>这是模拟数据</div>
-    <div>这是模拟数据</div>
+    <div class="text-black dark:text-white">
+      <h2>节点信息面板</h2>
+      <div>这是模拟数据</div>
+      <div>这是模拟数据</div>
+      <div>这是模拟数据</div>
+      <div>这是模拟数据</div>
+      <div>这是模拟数据</div>
+      <div>这是模拟数据</div>
+      <div>这是模拟数据</div>
+      <div>这是模拟数据</div>
+      <div>这是模拟数据</div>
+      <div>这是模拟数据</div>
+      <div>这是模拟数据</div>
+      <div>这是模拟数据</div>
+      <div>这是模拟数据</div>
+      <div>这是模拟数据</div>
+      <div>这是模拟数据</div>
+      <div>这是模拟数据</div>
+    </div>
   </el-card>
 </template>
